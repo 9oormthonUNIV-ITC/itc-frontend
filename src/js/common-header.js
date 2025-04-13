d96687f1cbbb6640cc9b8030ad8d169c74681752
seuchoi0531 <seuchoi0531@gmail.com>
2025-04-14 01:02:55 +0900

    :sparkles: feat: Update hamburger function

--- src/js/common-header.js
+++ src/js/common-header.js
@@ -1,12 +1,17 @@
 let sidenav_outside = document.querySelector(".sidenav-outside");
 let hamburger = document.querySelector(".hamburger");
 let show_mobile_nav_flag = false;
+let be_mobile_viewport_flag = false;
 sidenav_outside.addEventListener("click", mobileNavClose);
 hamburger.addEventListener("click", mobileNavToggle);
 window.addEventListener("resize", checkResize);
 function mobileNavToggle() {
-  if (show_mobile_nav_flag) mobileNavClose();
-  else mobileNavOpen();
+  if (show_mobile_nav_flag) {
+    mobileNavClose();
+  } else {
+    mobileNavOpen();
+  }
+  switchHamburgerIcon();
 }
 function mobileNavClose() {
   console.log("click!!");
@@ -25,7 +30,66 @@ function mobileNavOpen() {
 }
 function checkResize() {
   if (window.innerWidth >= 768) {
+    be_mobile_viewport_flag = true;
     show_mobile_nav_flag = false;
+    hamburger.style.display = "none";
+    console.log("hamburger is none");
+    console.log(hamburger.style.display);
     mobileNavClose();
+  }
+  if (window.innerWidth < 768) initializeHamburger();
+}
+
+function initializeHamburger() {
+  if (hamburger.style.display == "none") {
+    hamburger.style.display = "flex";
+    console.log("hamburger is none");
+    let x = hamburger.children[0];
+    let y = hamburger.children[1];
+    let z = hamburger.children[2];
+    x.style.animation = "none";
+    z.style.animation = "none";
+    y.style.visibility = "visible";
+    x.style.transition = "0 0";
+    x.style.rotate = "0deg";
+    x.style.scale = "1 1";
+    z.style.transition = "0 0";
+    z.style.rotate = "0deg";
+    z.style.scale = "1 1";
+  }
+}
+
+function switchHamburgerIcon() {
+  let x = hamburger.children[0];
+  let y = hamburger.children[1];
+  let z = hamburger.children[2];
+  if (!show_mobile_nav_flag) {
+    x.style.animation = "none";
+    z.style.animation = "none";
+    x.style.animation = "var(--animate-move-topbar-reverse)";
+    z.style.animation = "var(--animate-move-bottombar-reverse)";
+    x.style.animationPlayState = "running";
+    z.style.animationPlayState = "running";
+    setTimeout(() => {
+      y.style.visibility = "visible";
+    }, 150);
+    setTimeout(() => {
+      x.style.animationPlayState = "paused";
+      z.style.animationPlayState = "paused";
+    }, 300);
+  } else {
+    x.style.animation = "none";
+    z.style.animation = "none";
+    x.style.animation = "var(--animate-move-topbar)";
+    z.style.animation = "var(--animate-move-bottombar)";
+    x.style.animationPlayState = "running";
+    z.style.animationPlayState = "running";
+    setTimeout(() => {
+      y.style.visibility = "hidden";
+    }, 150);
+    setTimeout(() => {
+      x.style.animationPlayState = "paused";
+      z.style.animationPlayState = "paused";
+    }, 300);
   }
 }
